6787487db6afa7086b23edb5d59d8cd15356b4df
LiZhongpeng2 <lizhongpeng2@gmail.com>
2026-01-12 09:50:40 +0800

    更新：查询历史消息。删除会话接口

--- app/api/message.py
+++ app/api/message.py
@@ -1,6 +1,20 @@
-from IPython.terminal.embed import embed
 from fastapi import APIRouter, Depends
 from app.utils.JWTutils.authentication import verify_token
-from app.services.title_generator import generate_title
+from app.database.service.message import get_messages
+from app.database.service.session import check_session_owner
 
 router = APIRouter(prefix="/message", tags=["消息"])
+
+
+@router.get("/get-all-messages")
+async def get_all_message(session_id: int, user_id: str = Depends(verify_token)):
+    try:
+        if not check_session_owner(session_id, user_id):
+            return {"code": 403, "message": "无权访问此会话", "data": None}
+
+        result = get_messages(session_id)
+
+        return {"code": "200", "message": "获取成功", "data": result.data}
+
+    except Exception as e:
+        return {"code": "500", "message": f"获取失败，{e}", "data": None}
