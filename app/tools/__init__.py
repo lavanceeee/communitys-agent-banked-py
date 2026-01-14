9aa2232dd232f31df85070b9e8b1ed306167d075
Hzdgy <2870959239@qq.com>
2026-01-14 13:19:16 +0800

    邮件定时工具

--- app/tools/__init__.py
+++ app/tools/__init__.py
@@ -4,12 +4,14 @@ from app.tools.community.notification_tools import get_user_notifications
 from app.tools.community.privatemessage_tools import send_private_messages
 from app.tools.community.notification_tools import read_notification
 from app.tools.api.weather_tools import get_weather
+from app.tools.api.get_time_tools import get_time
 from app.tools.others.search import (
     web_search,
     wikipedia_search,
     toutiao_hot_news,
     search_domains_info,
 )
+from app.tools.api.scheduledEmail_tools import send_scheduled_email, delete_scheduled_email, get_scheduled_email
 
 # 未来如果有其他工具文件，继续在这里导入
 # from app.tools.wallet_tools import query_wallet_balance, transfer_money
@@ -26,5 +28,9 @@ all_tools = [
     wikipedia_search,
     toutiao_hot_news,
     search_domains_info,
+    send_scheduled_email,
+    get_time,
+    delete_scheduled_email,
+    get_scheduled_email,
     # 以后新增工具直接在这里添加
 ]
